8a18e209c4cd830ed13fa5549c59d756c93891c5
Krishna Mohan <krishanmohank974@gmail.com>
2026-02-03 22:54:33 +0530

    fix: changed version details, and problems
    Signed-off-by: Krishna Mohan <krishanmohank974@gmail.com>

--- src/components/tiptap-icons/magic-pencil-icon.tsx
+++ src/components/tiptap-icons/magic-pencil-icon.tsx
@@ -1,30 +1,30 @@
 
 import React from "react";
 
-const MagicPencilIcon = ({
-    className,
-}: {
-    className?: string;
-}) => {
-    return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className={className}
-        >
-            <path d="m12 3-1.912 5.813a2 2 0 0 1-1.275 1.275L3 12l5.813 1.912a2 2 0 0 1 1.275 1.275L12 21l1.912-5.813a2 2 0 0 1 1.275-1.275L21 12l-5.813-1.912a2 2 0 0 1-1.275-1.275L12 3Z" />
-            <path d="M5 3v4" />
-            <path d="M9 17v4" />
-            <path d="M3 5h4" />
-            <path d="M17 9h4" />
-        </svg>
-    );
-};
+// const MagicPencilIcon = ({
+//     className,
+// }: {
+//     className?: string;
+// }) => {
+//     return (
+//         <svg
+//             xmlns="http://www.w3.org/2000/svg"
+//             viewBox="0 0 24 24"
+//             fill="none"
+//             stroke="currentColor"
+//             strokeWidth="2"
+//             strokeLinecap="round"
+//             strokeLinejoin="round"
+//             className={className}
+//         >
+//             <path d="m12 3-1.912 5.813a2 2 0 0 1-1.275 1.275L3 12l5.813 1.912a2 2 0 0 1 1.275 1.275L12 21l1.912-5.813a2 2 0 0 1 1.275-1.275L21 12l-5.813-1.912a2 2 0 0 1-1.275-1.275L12 3Z" />
+//             <path d="M5 3v4" />
+//             <path d="M9 17v4" />
+//             <path d="M3 5h4" />
+//             <path d="M17 9h4" />
+//         </svg>
+//     );
+// };
 // I'm using a "sparkle" like star shape. Let's actually make it look like a pencil with stars.
 // Changing the path to be a pencil + sparkles.
 export const MagicPencilIcon2 = ({
